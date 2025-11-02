ce3d2de286c434ce94a1653ed8debe590eb27891
Noah Jutz <noah@noahjutz.com>
2025-11-02 10:47:16 +0100

    Add basic server
    untested, but should accept any incoming socket and immediately hang up

---
+++ ue4-1/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.util.concurrent.Executors;
+
+void main() {
+    final var pool = Executors.newFixedThreadPool(
+        Runtime.getRuntime().availableProcessors()
+    );
+    try (final var server = new ServerSocket()) {
+        while (true) {
+            try (final var socket = server.accept()) {
+                pool.execute(() -> {
+                    try (
+                        final var r = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream())
+                        );
+                        final var w = new PrintWriter(socket.getOutputStream())
+                    ) {} catch (IOException e) {}
+                });
+            }
+        }
+    } catch (IOException e) {}
+}
