da29a15c1af57fa42538820b97ca6462466848d1
Noah Jutz <noah@noahjutz.com>
2025-11-07 14:14:20 +0100

    Fix Socket Closed due to premature try-with-resources close

--- ue4-1/src/Server.java
+++ ue4-1/src/Server.java
@@ -7,64 +7,61 @@ void main() {
         final var server = new ServerSocket(1225)
     ) {
         while (true) {
-            try (final var socket = server.accept()) {
-                pool.execute(() -> {
-                    try (
-                        final var r = new DataInputStream(
-                            socket.getInputStream()
-                        );
-                        final var w = new BufferedOutputStream(
-                            socket.getOutputStream()
-                        )
-                    ) {
-                        final byte[] res = switch (Protocol.Decoder.parse(r)) {
-                            case Protocol.ReqRegister req -> {
-                                service.users.add(req.username());
-                                yield Protocol.Encoder.encode(
-                                    new Protocol.ResStatus(
-                                        req.id(),
-                                        Protocol.STATUS_OK
-                                    )
-                                );
-                            }
-                            case Protocol.ReqGet req -> {
-                                final var messages = service.messages
-                                    .stream()
-                                    .filter(msg ->
-                                        msg.to().equals(req.username())
-                                    )
-                                    .toList();
-                                yield Protocol.Encoder.encode(
-                                    new Protocol.ResGet(
-                                        req.id(),
-                                        Protocol.STATUS_OK,
-                                        messages
-                                    )
-                                );
-                            }
-                            case Protocol.ReqSend req -> {
-                                service.messages.add(req.message());
-                                yield Protocol.Encoder.encode(
-                                    new Protocol.ResStatus(
-                                        req.id(),
-                                        Protocol.STATUS_OK
-                                    )
-                                );
-                            }
-                            case null -> {
-                                // TODO reply
-                                throw new UnsupportedOperationException();
-                            }
-                            default -> throw new IllegalStateException();
-                        };
+            final var socket = server.accept();
+            pool.execute(() -> {
+                try (
+                    socket;
+                    final var r = new DataInputStream(socket.getInputStream());
+                    final var w = new BufferedOutputStream(
+                        socket.getOutputStream()
+                    )
+                ) {
+                    final byte[] res = switch (Protocol.Decoder.parse(r)) {
+                        case Protocol.ReqRegister req -> {
+                            service.users.add(req.username());
+                            yield Protocol.Encoder.encode(
+                                new Protocol.ResStatus(
+                                    req.id(),
+                                    Protocol.STATUS_OK
+                                )
+                            );
+                        }
+                        case Protocol.ReqGet req -> {
+                            final var messages = service.messages
+                                .stream()
+                                .filter(msg -> msg.to().equals(req.username()))
+                                .toList();
+                            yield Protocol.Encoder.encode(
+                                new Protocol.ResGet(
+                                    req.id(),
+                                    Protocol.STATUS_OK,
+                                    messages
+                                )
+                            );
+                        }
+                        case Protocol.ReqSend req -> {
+                            service.messages.add(req.message());
+                            yield Protocol.Encoder.encode(
+                                new Protocol.ResStatus(
+                                    req.id(),
+                                    Protocol.STATUS_OK
+                                )
+                            );
+                        }
+                        case null -> {
+                            // TODO reply
+                            throw new UnsupportedOperationException();
+                        }
+                        default -> throw new IllegalStateException();
+                    };
 
-                        w.write(res);
-                        w.flush();
-                    } catch (IOException e) {
-                        IO.println(e);
-                    }
-                });
-            }
+                    w.write(res);
+                    w.flush();
+                } catch (IOException e) {
+                    IO.println(e);
+                    e.printStackTrace();
+                }
+            });
         }
     } catch (IOException e) {
         IO.println(e);
