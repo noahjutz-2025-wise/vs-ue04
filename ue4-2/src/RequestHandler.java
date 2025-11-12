9379f941ec1cdd287e392406c582f3df126b9d18
Noah Jutz <noah@noahjutz.com>
2025-11-12 18:23:56 +0100

    Fix cases in RequestHandler
    Fixed AI Slop

--- ue4-2/src/RequestHandler.java
+++ ue4-2/src/RequestHandler.java
@@ -5,11 +5,9 @@ public class RequestHandler {
     public static void handle(Message request) {
         switch (request) {
             case Protocol.Open req -> {}
-            case Protocol.Close req -> {}
-            case Protocol.Ping req -> {}
-            case Protocol.Pong req -> {}
-            case Protocol.Error req -> {}
-            case Protocol.Unknown req -> {}
+            case Protocol.Exit req -> {}
+            case Protocol.Publ req -> {}
+            case Protocol.Admn req -> {}
         }
     }
 }
