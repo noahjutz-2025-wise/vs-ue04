48d0b80e884e31301089a6ea173fc81140bf68b6
Noah Jutz <noah@noahjutz.com>
2025-11-12 18:23:04 +0100

    Add method stubs

--- ue4-2/src/MessageService.java
+++ ue4-2/src/MessageService.java
@@ -9,4 +9,20 @@ public class MessageService {
     public void open(Socket socket, String username) {
         sockets.put(socket, username);
     }
+
+    public void exit(Socket socket) {
+        sockets.remove(socket);
+    }
+
+    public void publ(Socket socket, String message) {
+        // TODO
+    }
+
+    public void admn(Socket socket, String message) {
+        // TODO
+    }
+
+    public void show(String username, String message) {
+        // TODO
+    }
 }
