a4ccca0a4c0ca1049f890f04258aca8ed279ff7e
Noah Jutz <noah@noahjutz.com>
2025-11-12 18:39:01 +0100

    Update comments in MessageService

--- ue4-2/src/MessageService.java
+++ ue4-2/src/MessageService.java
@@ -7,22 +7,16 @@ public class MessageService {
     private final Map<Socket, String> sockets = new HashMap<>();
 
     public void open(Socket socket, String username) {
+        // TODO ADMN
         sockets.put(socket, username);
     }
 
     public void exit(Socket socket) {
+        // TODO ADMN
         sockets.remove(socket);
     }
 
     public void publ(Socket socket, String message) {
-        // TODO
-    }
-
-    public void admn(Socket socket, String message) {
-        // TODO
-    }
-
-    public void show(String username, String message) {
-        // TODO
+        // TODO SHOW
     }
 }
