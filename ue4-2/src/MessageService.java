8565b3b33ef1fc2e4aec4160011ebd76ed37ce63
Noah Jutz <noah@noahjutz.com>
2025-11-12 19:12:30 +0100

    Fix messages being sent back to sender

--- ue4-2/src/MessageService.java
+++ ue4-2/src/MessageService.java
@@ -16,7 +16,7 @@ public class MessageService {
             socketWriters.put(socket, out);
             sockets.put(socket, username);
             final var msg = new Protocol.Admn("Welcome, " + username + "!");
-            dispatch(msg);
+            dispatch(msg, null);
         } catch (IOException e) {}
     }
 
@@ -25,7 +25,7 @@ public class MessageService {
         final var msg = new Protocol.Admn(
             "Goodbye, " + sockets.get(socket) + "!"
         );
-        dispatch(msg);
+        dispatch(msg, null);
         var writer = socketWriters.remove(socket);
         if (writer != null) {
             writer.close();
@@ -36,11 +36,12 @@ public class MessageService {
     public void publ(Socket socket, String message) {
         IO.println("PUBL: " + sockets.get(socket) + ": " + message);
         final var msg = new Protocol.Show(sockets.get(socket), message);
-        dispatch(msg);
+        dispatch(msg, socket);
     }
 
-    private void dispatch(Protocol.Message msg) {
+    private void dispatch(Protocol.Message msg, Socket ignore) {
         for (final var socket : sockets.keySet()) {
+            if (socket == ignore) continue;
             var out = socketWriters.get(socket);
             if (out != null && !out.checkError()) {
                 final var enc = switch (msg) {
