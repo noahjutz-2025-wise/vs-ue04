3495c925c4ca84b2e6d958229430a2ebdfaf5eae
Noah Jutz <noah@noahjutz.com>
2025-11-12 19:08:50 +0100

    Fix socket being closed on first message

--- ue4-2/src/MessageService.java
+++ ue4-2/src/MessageService.java
@@ -7,12 +7,17 @@ import java.util.Map;
 public class MessageService {
 
     private final Map<Socket, String> sockets = new HashMap<>();
+    private final Map<Socket, PrintWriter> socketWriters = new HashMap<>();
 
     public void open(Socket socket, String username) {
         IO.println("OPEN: " + username);
-        final var msg = new Protocol.Admn("Welcome, " + username + "!");
-        dispatch(msg);
-        sockets.put(socket, username);
+        try {
+            final var out = new PrintWriter(socket.getOutputStream(), true);
+            socketWriters.put(socket, out);
+            sockets.put(socket, username);
+            final var msg = new Protocol.Admn("Welcome, " + username + "!");
+            dispatch(msg);
+        } catch (IOException e) {}
     }
 
     public void exit(Socket socket) {
@@ -21,6 +26,10 @@ public class MessageService {
             "Goodbye, " + sockets.get(socket) + "!"
         );
         dispatch(msg);
+        var writer = socketWriters.remove(socket);
+        if (writer != null) {
+            writer.close();
+        }
         sockets.remove(socket);
     }
 
@@ -32,14 +41,16 @@ public class MessageService {
 
     private void dispatch(Protocol.Message msg) {
         for (final var socket : sockets.keySet()) {
-            try (var out = new PrintWriter(socket.getOutputStream())) {
+            var out = socketWriters.get(socket);
+            if (out != null && !out.checkError()) {
                 final var enc = switch (msg) {
                     case Protocol.Admn m -> Protocol.encode(m);
                     case Protocol.Show m -> Protocol.encode(m);
                     default -> throw new IllegalArgumentException();
                 };
                 out.println(enc);
-            } catch (IOException e) {}
+                out.flush();
+            }
         }
     }
 }
