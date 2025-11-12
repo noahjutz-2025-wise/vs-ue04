2b77229bd74d19522d3370a334e6b9650caeb4a5
Noah Jutz <noah@noahjutz.com>
2025-11-12 18:21:19 +0100

    Dont track sockets in server

--- ue4-2/src/Server.java
+++ ue4-2/src/Server.java
@@ -3,13 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 
-List<Socket> sockets = new ArrayList<>();
-
 void main() {
     try (final var server = new ServerSocket(2345)) {
         while (true) {
             final var socket = server.accept();
-            sockets.add(socket);
             new Thread(() -> {
                 try (
                     final var in = new BufferedReader(
@@ -26,7 +23,6 @@ void main() {
                 }
             })
                 .start();
-            sockets.remove(socket);
         }
     } catch (IOException e) {
         e.printStackTrace();
